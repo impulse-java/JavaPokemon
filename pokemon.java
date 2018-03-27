d6706d01bbc1959b951ae4b9a6b27011e887f166
impulse-java <37848934+impulse-java@users.noreply.github.com>
2018-03-27 15:05:22 -0500

    Create pokemon.java

---
+++ pokemon.java
@@ -0,0 +1,22 @@
+package pokemon;
+
+import java.util.Scanner;
+import java.util.Random;
+
+class pokemon {
+    private String charizard;
+    private String pika;
+    private int health;
+    private String moves;
+    
+    
+    
+}
+
+public class Pokemon {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
