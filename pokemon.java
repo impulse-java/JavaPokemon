7f7edf6851d3c3693111e21a6bcb1fe9b5bf42b3
impulse-java <37848934+impulse-java@users.noreply.github.com>
2018-03-28 13:32:07 -0500

    Update pokemon.java

--- pokemon.java
+++ pokemon.java
@@ -3,11 +3,46 @@ package pokemon;
 import java.util.Scanner;
 import java.util.Random;
 
+class moveSet {
+  
+  private int flamethrower;
+  private int fly;
+  private int smokescreen;
+  private int struggle;
+  
+  public moveSet() throws InterruptedException {
+    flamethrower = 25;
+    fly = 20;
+    smokescreen = 5;
+    struggle = 10;
+    System.out.println("Charizard's turn! ");
+    int moveRand = (int) (Math.random() * 3 + 1);
+    if(moveRand == 1) {
+      Thread.sleep(1000);
+      
+      System.out.println();
+      System.out.println("Charizard used Flamethrower! ");
+    }
+    if(moveRand == 2) {
+      Thread.sleep(1000);
+      
+      System.out.println();
+      System.out.println("Charizard used Fly! ");
+    }
+    if(moveRand == 3) {
+      Thread.sleep(1000);
+      
+      System.out.println();
+      System.out.println("Charizard used Smokescreen!");
+    }
+  }
+}
+
 class pokemon {
   private final String CHARIZARD;
   private String pika;
-  private int c_health;
-  private int p_health;
+  private static int c_health;
+  private static int p_health;
   private String moves;
   private int thunderbolt;
   private int tackle;
@@ -47,16 +82,21 @@ class pokemon {
     switch(chooseMove) {
       case 1:
         System.out.println(pika + " used Thunderbolt!");
+        System.out.println();
+        Thread.sleep(1000);
         System.out.println("It did " + thunderbolt + " damage!");
         
         Thread.sleep(1000);
         
         c_health = c_health - thunderbolt;
         System.out.println("Charizard has " + c_health + " health left");
+        moveSet atk = new moveSet();
         return;
         
       case 2:
         System.out.println(pika + " used Tackle!");
+        System.out.println();
+        Thread.sleep(1000);
         System.out.println("It did " + tackle + " damage!");
         
         Thread.sleep(1000);
@@ -67,11 +107,15 @@ class pokemon {
         
       case 3:
         System.out.println(pika + " used Double-Team!");
+        System.out.println();
+        Thread.sleep(1000);
         System.out.println("Pikachu's speed went up by " + dteam + "!");
         return;
         
       case 4:
         System.out.println(pika + " used Struggle!");
+        System.out.println();
+        Thread.sleep(1000);
         System.out.println("It did " + struggle + " damage!");
         
         Thread.sleep(1000);
@@ -84,7 +128,6 @@ class pokemon {
         System.out.println("Error!");
         return;
     }
-    
   }
 }
 
